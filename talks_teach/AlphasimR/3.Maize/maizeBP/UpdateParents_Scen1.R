f48f517dd1ea3080219bfcd75b805f337a9abc98
Marco Antônio Peixoto <59318360+marcopxt@users.noreply.github.com>
2023-11-14 09:23:26 -0500

    Add files via upload

---
+++ talks_teach/AlphasimR/3.Maize/maizeBP/UpdateParents_Scen1.R
@@ -0,0 +1,7 @@
+#Sets new parents for inbreds
+
+# Male heterotic group
+MaleParents = selectInd(c(MaleInbredYT4,MaleInbredYT3),nParents)
+
+# Female heterotic group
+FemaleParents = selectInd(c(FemaleInbredYT4,FemaleInbredYT3),nParents)
---
+++ talks_teach/AlphasimR/3.Maize/maizeBP/UpdateParents_Scen2.R
@@ -0,0 +1,8 @@
+#Sets new parents for inbreds
+
+# Male heterotic group
+MaleParents = selectInd(MaleYT2,nParents)
+
+# Female heterotic group
+FemaleParents = selectInd(FemaleYT2,nParents)
+
